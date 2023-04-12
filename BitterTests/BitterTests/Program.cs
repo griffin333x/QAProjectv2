c7032693179a9a18c2e7cdb60ca94a239acb5d76
griffin333x <gbell01@mynbcc.ca>
2023-04-12 16:42:02 -0300

    Added Console application for Bitter Tests

---
+++ BitterTests/BitterTests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Security.Policy;
+
+
+
+namespace BitterTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
