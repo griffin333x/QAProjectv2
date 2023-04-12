85e14010ecf9a30f23717312c0bb7c95edc53fff
zMorto <116381433+zMorto@users.noreply.github.com>
2023-04-12 16:59:50 -0300

    Adding classes as test because wasnt working before

--- BitterTests/BitterTests/SiteTest.cs
+++ BitterTests/BitterTests/SiteTest.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Security.Policy;
+
 
 namespace BitterTests
 {
--- BitterTests/BitterTests/SiteWebElements.cs
+++ BitterTests/BitterTests/SiteWebElements.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Security.Policy;
+
 
 namespace BitterTests
 {
-    internal class SiteWebElement
+    internal class Class1
     {
     }
 }
